9d9f5c4d46e303bf2da3af98db1f8c230f145210
abaxsoraszem <csaba.meszaros@partner.commerzbank.com>
2014-11-06 18:45:29 +0100

    fix starting the free version

--- anon-gui/src/main/scripts/start.bat
+++ anon-gui/src/main/scripts/start.bat
@@ -1,1 +1,3 @@
-java -Dspring.profiles.active=derby_embedded -jar anon-gui-0.1-SNAPSHOT.war 
+start java -Dspring.profiles.active=derby_embedded -jar ..\..\..\target\anon-gui-0.1-SNAPSHOT.war
+sleep 5
+start iexplore http://localhost:8080/anon-gui/pages/cockpit.jsf
