0ef662b7774f939109b8f3deb0d2cb4b60f7a129
abaxsoraszem <abax.soraszem@gmail.com>
2015-01-26 13:49:07 +0100

    final release preparations for free anonimecs.com release

--- anon-build/scripts/startFree.bat
+++ anon-build/scripts/startFree.bat
@@ -5,7 +5,7 @@ cd %~dp0
 start java -Dspring.profiles.active=free_edition -jar anonimecsFree.war
 
 echo "waiting to start the app"
-ping 192.0.2.2 -n 1 -w 15000 > nul
+timeout /T 15  > nul
 echo "application started"
 
-start iexplore http://localhost:8080/anon-gui/connect/
+start iexplore http://localhost:8080/anon/pages/connection/connectionAdd.jsf
