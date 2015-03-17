21e880e23d6fe43ff96eabe3ec7124feb69df707
abaxsoraszem <abax.soraszem@gmail.com>
2015-03-17 13:43:36 +0100

    scripts for jenkins

--- anon-persistence/src/main/resources/sql/unix_create_tables.sql
+++ anon-persistence/src/main/resources/sql/unix_create_tables.sql
@@ -2,16 +2,15 @@ CONNECT 'jdbc:derby://localhost:1527/anon;user=app;password=anon;create=true';
 SET SCHEMA APP;
 
 CREATE TABLE SecurityUser (
-	ID INT NOT NULL GENERATED ALWAYS AS IDENTITY,
-	USERNAME VARCHAR(255) NOT NULL,
-	PASSWORD VARCHAR(255) NOT NULL,
-	NAME VARCHAR(255),
-	SURNAME VARCHAR(255),
-	ENABLED VARCHAR(1) NOT NULL,
-	ENCRYPTED VARCHAR(1) DEFAULT NULL,
-	CONSTRAINT primary_key_SecurityUser PRIMARY KEY (ID)
+  ID INT NOT NULL GENERATED ALWAYS AS IDENTITY,
+  USERNAME VARCHAR(255) NOT NULL,
+  PASSWORD VARCHAR(255) NOT NULL,
+  NAME VARCHAR(255),
+  SURNAME VARCHAR(255),
+  ENABLED VARCHAR(1) NOT NULL,
+  ENCRYPTED VARCHAR(1) DEFAULT NULL,
+  CONSTRAINT primary_key_SecurityUser PRIMARY KEY (ID)
 );
-
 
 CREATE TABLE SecurityUserRoles (
 	ID INT NOT NULL GENERATED ALWAYS AS IDENTITY,
@@ -21,19 +20,31 @@ CREATE TABLE SecurityUserRoles (
 	CONSTRAINT foreign_key_SecurityUser FOREIGN KEY (USER_ID) REFERENCES SecurityUser (ID)
 );
 
-
-CREATE TABLE DatabaseConfig (
+CREATE TABLE DatabaseConnection (	
 	ID INT NOT NULL GENERATED ALWAYS AS IDENTITY,
-	URL VARCHAR(255) NOT NULL,
-	DEFAULTSCHEMA VARCHAR(255) NOT NULL,
+  	URL VARCHAR(255) NOT NULL,
   	LOGIN VARCHAR(255) NOT NULL,
   	PASSWORD VARCHAR(255) NOT NULL,
-	VENDOR VARCHAR(255) NOT NULL,
-	VERSION VARCHAR(255),
- 	GUINAME VARCHAR(255) not null unique,
-	CONSTRAINT primary_key_DATABASECONFIG PRIMARY KEY (ID)
+  	VENDOR VARCHAR(255) NOT NULL,
+  	VERSION VARCHAR(255),
+    GUINAME VARCHAR(255) not null,
+   	SecurityUser_ID INT NOT NULL,
+	CONSTRAINT fk_SecurityUser_pk FOREIGN KEY (SecurityUser_ID) REFERENCES SecurityUser (ID),
+  	CONSTRAINT primary_key_DatabaseConnection PRIMARY KEY (ID),
+  	CONSTRAINT DatabaseConnection_UC UNIQUE (GUINAME, SecurityUser_ID)
 );
 
+CREATE TABLE DatabaseConfig (	
+	ID INT NOT NULL GENERATED ALWAYS AS IDENTITY,
+  	DEFAULTSCHEMA VARCHAR(255) NOT NULL,
+    CONFIGURATIONNAME VARCHAR(255) not null,
+    DatabaseConnection_ID INT NOT NULL,
+   	SecurityUser_ID INT NOT NULL,
+	CONSTRAINT fk_SecurityUser_pk2 FOREIGN KEY (SecurityUser_ID) REFERENCES SecurityUser (ID),
+  	CONSTRAINT primary_key_DATABASECONFIG PRIMARY KEY (ID),
+    CONSTRAINT fk_DatabaseConfig_DatabaseConnection FOREIGN KEY (DatabaseConnection_ID) REFERENCES DatabaseConnection (ID),
+  	CONSTRAINT DatabaseConfig_UC UNIQUE (CONFIGURATIONNAME, SecurityUser_ID)
+);
 
 create table AnonymisationMethodData (
     ID INT NOT NULL GENERATED ALWAYS AS IDENTITY,
@@ -44,7 +55,6 @@ create table AnonymisationMethodData (
   	CONSTRAINT primary_key_AnonymisationMethodData PRIMARY KEY (ID),
     CONSTRAINT fk_AnonymisationMethodData_DatabaseConfig FOREIGN KEY (DATABASECONFIG_ID) REFERENCES DatabaseConfig (ID)
 );
-
 
 create table AnonymisedColumnData (
     ID INT NOT NULL GENERATED ALWAYS AS IDENTITY,
@@ -58,13 +68,11 @@ create table AnonymisedColumnData (
 
 );
 
-
 ALTER TABLE AnonymisedColumnData
 	ADD CONSTRAINT unique_col_anonymisation
-	UNIQUE (COLUMNNAME, TABLENAME, SCHEMANAME, AnonymisationMethodData_ID);
-	 
+	 UNIQUE (COLUMNNAME, TABLENAME, SCHEMANAME, AnonymisationMethodData_ID);
 
-create table MappingRuleData (
+CREATE TABLE MappingRuleData (
     ID INT NOT NULL GENERATED ALWAYS AS IDENTITY,
     MAPPINGRULETYPE VARCHAR(30) NOT NULL,
     BOUNDARY VARCHAR(100),
@@ -75,14 +83,16 @@ create table MappingRuleData (
     
 );
 
-
-create table MappingDefaultData (
+CREATE TABLE MappingDefaultData (
     ID INT NOT NULL GENERATED ALWAYS AS IDENTITY,
     AnonymisationMethodData_ID int,
     DEFAULTVALUE VARCHAR(100),
   	CONSTRAINT primary_key_MappingDefaultData PRIMARY KEY (ID),
     CONSTRAINT fk_MappingDefaultData_AnonymisationMethodData FOREIGN KEY (AnonymisationMethodData_ID) REFERENCES AnonymisationMethodData (ID) ON DELETE CASCADE
 );
+
+
+
 
 
 create table ExecutionData(
@@ -93,7 +103,6 @@ create table ExecutionData(
     endTime TIMESTAMP,
     status varchar(20) not null
 );
-
 
 create table ExecutionMethodData(
     ID INT NOT NULL GENERATED ALWAYS AS IDENTITY primary key,
@@ -106,7 +115,6 @@ create table ExecutionMethodData(
     CONSTRAINT fk_ExecutionMethod_Method FOREIGN KEY (METHOD_ID) REFERENCES AnonymisationMethodData (ID)
 );
 
-
 create table ExecutionColumnData(
     ID INT NOT NULL GENERATED ALWAYS AS IDENTITY primary key,
     RESULTTEXT VARCHAR(1000),
@@ -118,7 +126,6 @@ create table ExecutionColumnData(
     CONSTRAINT fk_ExecCol_Col FOREIGN KEY (COLUMN_ID) REFERENCES AnonymisedColumnData (ID)
 );
 
-
 create table ExecutionMessageData(
     ID INT NOT NULL GENERATED ALWAYS AS IDENTITY primary key,
     MessageText VARCHAR(1000) not null,
@@ -127,18 +134,8 @@ create table ExecutionMessageData(
 );
 
 
-create table User_Database(
-	USER_ID INT NOT NULL,
-	DATABASE_ID INT NOT NULL,
-	PRIMARY KEY (USER_ID, DATABASE_ID),
-	CONSTRAINT fk_SecurityUser_pk FOREIGN KEY (USER_ID) REFERENCES SecurityUser (ID),
-	CONSTRAINT fk_DatabaseConfig_pk FOREIGN KEY (DATABASE_ID) REFERENCES DatabaseConfig (ID)
-);
+INSERT INTO APP.SECURITYUSER(USERNAME, PASSWORD, NAME, SURNAME, ENABLED, ENCRYPTED) 
+	VALUES('admin', '123456', 'BuiltIn', 'User', 'Y', 'N');
 
-
-INSERT INTO SecurityUser(USERNAME, PASSWORD, NAME, SURNAME, ENABLED, ENCRYPTED) 
-	VALUES('admin', '123456', 'Admin', 'Fellow', 'Y', 'N');
-
-	
-INSERT INTO SecurityUserRoles(USER_ID, ROLE) 
+INSERT INTO APP.SECURITYUSERROLES(USER_ID, ROLE) 
 	VALUES( 1, 'ROLE_ADMIN');
