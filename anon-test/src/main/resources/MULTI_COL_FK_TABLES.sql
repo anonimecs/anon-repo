d24232dee933b4883ce4af8bf0bb8c9b1fe40103
abaxsoraszem <abax.soraszem@gmail.com>
2015-03-24 09:38:55 +0100

    enhanced test for FK constraint manager

--- anon-test/src/main/resources/MULTI_COL_FK_TABLES.sql
+++ anon-test/src/main/resources/MULTI_COL_FK_TABLES.sql
@@ -17,3 +17,18 @@ CONSTRAINT my_fk    FOREIGN KEY (COL1_REF, COL2_REF)    REFERENCES TMP_TABLE_A (
 insert into TMP_TABLE_B values ('AAA', 'AAA');
 insert into TMP_TABLE_B values ('BBB', 'BBB');
 insert into TMP_TABLE_B values ('CCC', 'CCC');
+
+create table TMP_TABLE_C(
+COL1 varchar(50) not null,
+primary key(COL1)
+);
+
+create table TMP_TABLE_D(
+COL1_REF varchar(50) not null,
+CONSTRAINT my_fk2    FOREIGN KEY (COL1_REF)    REFERENCES TMP_TABLE_C (COL1) 
+);
+
+create table TMP_TABLE_E(
+COL1 varchar(50) not null,
+primary key(COL1)
+);
