e96d86c13df98ef3b714d5b45add078a9e2e00c1
abaxsoraszem <abax.soraszem@gmail.com>
2015-03-26 23:00:51 +0100

    enhanced exec messages

--- anon-core/src/main/java/org/anon/data/ExecutionMessage.java
+++ anon-core/src/main/java/org/anon/data/ExecutionMessage.java
@@ -1,13 +1,21 @@
 package org.anon.data;
 
+import java.util.Date;
+
 public class ExecutionMessage {
 
+	private Date createDate;
 	private String message;
 	private Integer rowCount;
 	public ExecutionMessage(String message, Integer rowCount) {
 		super();
 		this.message = message;
 		this.rowCount = rowCount;
+		this.createDate = new Date();
+	}
+	
+	public Date getCreateDate() {
+		return createDate;
 	}
 	
 	@Override
