7b596b12cb92f2bb53a8bdc10e7bf7c8e570362c
andre <mail@me.com>
2015-04-17 14:16:37 +0200

    added free windows tomcat version

--- anon-build/scripts/startFree.bat
+++ anon-build/scripts/startFree.bat
@@ -1,11 +1,14 @@
 @echo off
 
-cd %~dp0
+set "CURRENT_DIR=%cd%"
+set "CATALINA_HOME=%CURRENT_DIR%\app"
+set "APP_EXECUTABLE=%CATALINA_HOME%\bin\catalina.bat"
+set "CATALINA_OPTS=-Xms1024M -Xmx2048M -Dspring.profiles.active=free_edition"
 
-start java -Dspring.profiles.active=free_edition -jar anonimecsFree.war
+call "%APP_EXECUTABLE%" start
 
 echo "waiting to start the app"
 timeout /T 25  > nul
 echo "application started"
 
-start iexplore http://localhost:8080/anon/pages/connection/connectionAdd.jsf
+start iexplore http://localhost:8080/anon
---
+++ anon-build/scripts/stopFree.bat
@@ -0,0 +1,10 @@
+@echo off
+
+set "CURRENT_DIR=%cd%"
+set "CATALINA_HOME=%CURRENT_DIR%\app"
+set "APP_EXECUTABLE=%CATALINA_HOME%\bin\catalina.bat"
+
+call "%APP_EXECUTABLE%" stop
+echo "waiting to stop the app"
+timeout /T 5  > nul
+echo "application stoped"
