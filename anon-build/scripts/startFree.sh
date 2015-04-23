23648cfc8ae81eac8f0ef5e1d95b72e7852ed040
andre <mail@me.com>
2015-04-23 15:51:00 +0200

    new unix/linux shell scripts

--- anon-build/scripts/startFree.sh
+++ anon-build/scripts/startFree.sh
@@ -1,23 +1,60 @@
-#!/bin/sh
+#!/bin/bash
+#
+# Anonimecs       start script
+#
+#                                                                                                                           
+#JAVA_HOME=
+                                                                                                                   
+# DO NOT CHANGE BELOW                                                                                                                                                      
+APPSERVER_LOGIN="http://`hostname`:8080/anon"
+                                                                                                                                               
+# CHECK FOR JAVA_HOME OR JRE_HOME                                                                                                                                                      
+darwin=false
+case "`uname`" in
+Darwin*) darwin=true;;
+esac
+                                                                                                                                                            
+if [ -z "$JAVA_HOME" ]; then
+        if $darwin; then
+                if [ -d "/System/Library/Frameworks/JavaVM.framework/Versions/CurrentJDK/Home" ]; then
+                JAVA_HOME="/System/Library/Frameworks/JavaVM.framework/Home"
+        fi
+        else
+                JAVA_PATH=`which java 2>/dev/null`
+                if [ "x$JAVA_PATH" != "x" ]; then
+                        JAVA_HOME=`dirname $JAVA_PATH 2>/dev/null`
+                fi
+        fi
+        if [ -z "$JAVA_HOME" ]; then
+                echo "The JAVA_HOME environment variable is not defined"
+                exit 1
+        fi
+fi
+if [ -z "$_RUNJAVA" ]; then
+  _RUNJAVA="$JAVA_HOME"/bin/java
+fi
+if [ ! -x "$_RUNJAVA" ] ; then
+  echo "Error: JAVA is not defined correctly."
+  echo "Cannot execute $_RUNJAVA"
+  exit 1
+fi
 
-#test the java version
-mkfifo mypipe
-if [ ` java -version 2>mypipe|grep -c "java version.*1\.7" mypipe` -eq 0 ]; then echo "java 7 is required to run this application"; rm mypipe; exit; fi
-rm mypipe
+# SET APP FOLDERS                                                                                                                                                        
+CURRENT_DIR=$(pwd)
+SCRIPT_DIR=$(dirname $0)
+cd $SCRIPT_DIR
+INSTALL_DIR="`pwd`"
+                                                                                                                                                            
+echo "INSTALL DIR: $INSTALL_DIR";
+echo "USING JAVA: $_RUNJAVA";
+                                                                                                                                                            
+LOGFILE=$INSTALL_DIR/anonimecs.log
 
-
-echo "logs are in anonimecs.log"
-
-CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $CURRENT_DIR
-
-export CATALINA_HOME=%CURRENT_DIR%/app
-export APP_EXECUTABLE=%CATALINA_HOME%/bin/startup.sh
-export CATALINA_OPTS=-Xms256M -Xmx1024M -Dspring.profiles.active=free_edition -Dderby.dir=%CURRENT_DIR%
-
-$APP_EXECUTABLE > anonimecs.log 2>&1 &
-echo "waiting to start the application"
-sleep 25
-echo "application started"
-
-echo "go to http://`hostname`:8080/anon"
+# START TOMCAT SERVER
+export $JAVA_HOME
+export CATALINA_HOME=$INSTALL_DIR/app
+export CATALINA_BASE=$CATALINA_HOME
+export CATALINA_EXEC=$CATALINA_HOME/bin/startup.sh
+export CATALINA_OPTS="-Xms256M -Xmx2048M -Dspring.profiles.active=enterprise_edition -Dderby.dir=$INSTALL_DIR"
+                                                                                                                                                            
+nohup $CATALINA_EXEC
