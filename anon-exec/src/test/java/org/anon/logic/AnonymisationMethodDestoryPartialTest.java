f5c203583fe9b8ca887f861af25b90286130f7b5
abaxsoraszem <abax.soraszem@gmail.com>
2015-04-28 13:32:24 +0200

    Test for #3

--- anon-exec/src/test/java/org/anon/logic/AnonymisationMethodDestoryPartialTest.java
+++ anon-exec/src/test/java/org/anon/logic/AnonymisationMethodDestoryPartialTest.java
@@ -12,6 +12,8 @@ import org.anon.data.DatabaseTableInfo;
 import org.anon.exec.BaseExec;
 import org.anon.exec.TwoTestTablesCreator;
 import org.anon.logic.map.TwoTablesAllDbTest;
+import org.anon.service.where.WhereConditionBuilder;
+import org.anon.service.where.WhereConditionBuilder.Applicability;
 import org.anon.vendor.DatabaseSpecifics;
 import org.junit.Assert;
 import org.junit.Test;
@@ -22,6 +24,8 @@ public class AnonymisationMethodDestoryPartialTest extends TwoTablesAllDbTest{
 		super(schema, database, testTablesCreator);
 	}
 
+	WhereConditionBuilder whereConditionBuilder = new WhereConditionBuilder();
+	
 	
 	MethodFactory methodFactory = new MethodFactory();
 	
@@ -33,13 +37,37 @@ public class AnonymisationMethodDestoryPartialTest extends TwoTablesAllDbTest{
 
 		AnonymisationMethodDestory anonymisationMethodDestory = (AnonymisationMethodDestory)methodFactory.createMethod(AnonymizationType.DESTROY, database.getDatabaseSpecifics());
 		AnonymisedColumnInfo column = getTestAnonimisedColumnInfo("COL1", "VARCHAR2", "TMP_TABLE_A3", anonymisationMethodDestory, database.getDatabaseSpecifics(), anonConfig);
-		column.setWhereCondition("COL1 = 'AAA'");
+		String whereCondition = whereConditionBuilder.build(Applicability.APPLY, "COL1 = 'AAA'");
+		column.setWhereCondition(whereCondition);
 		
 		anonymisationMethodDestory.addColumn(column);
 		
 		runConfig(anonConfig, anonymisationMethodDestory);
 		
 		List<Map<String, Object>> res = loadFromDb("select distinct COL1 from TMP_TABLE_A3");
+		Assert.assertEquals(3,  res.size());
+	}
+
+	@Test
+	public void testSimpleWhereWithReferencingTable() throws Exception {
+
+		AnonConfig anonConfig = new AnonConfig();
+		anonConfig.init();
+
+		AnonymisationMethodDestory anonymisationMethodDestory = (AnonymisationMethodDestory)methodFactory.createMethod(AnonymizationType.DESTROY, database.getDatabaseSpecifics());
+		AnonymisedColumnInfo column = getTestAnonimisedColumnInfo("COL1", "VARCHAR2", "TMP_TABLE_A3", anonymisationMethodDestory, database.getDatabaseSpecifics(), anonConfig);
+		String whereCondition = whereConditionBuilder.build(Applicability.APPLY, "COL1 = 'AAA'");
+		column.setWhereCondition(whereCondition);
+		anonymisationMethodDestory.addColumn(column);
+		
+		AnonymisedColumnInfo referencingColumn = getTestAnonimisedColumnInfo("COL1_REF", "VARCHAR2", "TMP_TABLE_B3", anonymisationMethodDestory, database.getDatabaseSpecifics(), anonConfig);
+		String whereCondition2 = whereConditionBuilder.buildForRelatedTable(Applicability.APPLY, "COL1 = 'AAA'", referencingColumn, column);
+		referencingColumn.setWhereCondition(whereCondition2);
+		anonymisationMethodDestory.addColumn(referencingColumn);
+		
+		runConfig(anonConfig, anonymisationMethodDestory);
+		
+		List<Map<String, Object>> res = loadFromDb("select distinct COL1_REF from TMP_TABLE_B3");
 		Assert.assertEquals(3,  res.size());
 	}
 
